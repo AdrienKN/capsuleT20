192aceb258b8bbb4559fd8930fa84681513ec562
AdrienKN <adrien.knobloch@gmail.com>
2024-02-18 01:50:33 +0100

    add config js

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,30 @@
+export default {
+    base: '/capsuleT20/',
+    ignoreDeadLinks: true,
+    title: "Promo T20",
+    description: "Pour ne jamais oublier",
+    themeConfig: {
+      // https://vitepress.dev/reference/default-theme-config
+      nav: [
+        { text: 'Menu', link: '/' },
+        { text: 'Sommaire',
+        items: [
+          { text: 'Capsule temporelle', link: '/map' },
+          { text: 'Drives photos', link: '/api-examples' }
+      ]}],
+      sidebar: [
+        {
+          text: 'Sommaire',
+          items: [
+            { text: 'Capsule temporelle', link: '/map' },
+            { text: 'Drives photos', link: '/api-examples' }
+          ]
+        }
+      ],
+  
+      socialLinks: [
+        { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      ]
+    }
+  }
+  
